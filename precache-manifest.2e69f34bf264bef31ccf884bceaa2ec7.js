3924eaa42b836c5c793ad260cfe68307a69ca0b3
Vterebenin <niceserver11@mail.ru>
2019-07-22 00:39:02 +0300

    Updates

---
+++ precache-manifest.2e69f34bf264bef31ccf884bceaa2ec7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "06dee50310a110ce2ede",
+    "url": "/graphql-course/static/css/main.7c88c6d0.chunk.css"
+  },
+  {
+    "revision": "06dee50310a110ce2ede",
+    "url": "/graphql-course/static/js/main.0aa7f47d.chunk.js"
+  },
+  {
+    "revision": "645a81e4dbef151e923b",
+    "url": "/graphql-course/static/js/runtime~main.3462b002.js"
+  },
+  {
+    "revision": "bbaf6b83380b587c62fe",
+    "url": "/graphql-course/static/js/2.4f0c7947.chunk.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/graphql-course/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "ab04d08edd756244d06cf2e27c3c4436",
+    "url": "/graphql-course/index.html"
+  }
+];
